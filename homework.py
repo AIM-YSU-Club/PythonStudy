de1e96be97166a916c050da527e0f8f5f46d88ef
ysm-y <sjchin124@naver.com>
2026-06-02 16:36:25 +0900

    서준이형 때리기

--- homework.py
+++ homework.py
@@ -21,6 +21,17 @@ class Student():
     def setBestFriend(self, bf: "Student"):
         self.bestFriend = bf
 
+    # 윤성민 / 타겟을 받아서 이름이 홍서준이것을 검사를 하고 홍서준이 맞으면 때립니다. 그러면 몸무게가 100kg이 빠집니다. 근데 몸무게가 음수가 될수 없어서 100을 뺸 결과가 0보다 작으면 10kg설정 합니다.
+    def hitOtherStudent(self, target: "Student"):
+        if target.name == '홍서준':
+            print("홍서준을 때렸습니다.")
+        
+        target.bodyWeight -= 100 
+        if target.bodyWeight < 0:
+            target.bodyWeight = 10
+        
+
+        
     # 여기서부터 각자 메소드를 하나씩 만들어보세요!
 
 # 우재성 / jaeseong 객체와 denji 객체를 생성합니다.
